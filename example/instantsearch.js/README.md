76e262ab97aa447dff751592a9891a218bae734c
Marie-Laure Thuret <marielaure.thuret@algolia.com>
2018-02-01 11:28:51 +0100

    docs: add example

---
+++ example/instantsearch.js/README.md
@@ -0,0 +1,26 @@
+# Example
+
+## Description
+
+This is an example of an experimentation using InstantSearch.js. 
+
+[See it live on Codesandobox](https://codesandbox.io/s/github/algolia/instantsearch-labs/tree/master/example/instantsearch.js)
+
+## Running the experimentation locally
+
+```
+yarn
+yarn start
+```
+
+## Tutorial
+
+### Step1 
+
+### Step2
+
+### ...
+
+## Known limitations
+
+None
---
+++ example/instantsearch.js/src/index.js
@@ -0,0 +1,44 @@
+/* global instantsearch */
+
+import './index.css';
+
+const search = instantsearch({
+  appId: 'latency',
+  apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
+  indexName: 'instant_search',
+  searchParameters: {
+    hitsPerPage: 8,
+  },
+});
+
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: document.querySelector('#products'),
+    templates: {
+      item: '{{{_highlightResult.name.value}}}',
+    },
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: document.querySelector('#searchBox'),
+    placeholder: 'Search for products',
+    autofocus: false /* Only to avoid live preview taking focus */,
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.refinementList({
+    container: document.querySelector('#brand'),
+    attributeName: 'brand',
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.pagination({
+    container: document.querySelector('#pagination'),
+  })
+);
+
+search.start();
