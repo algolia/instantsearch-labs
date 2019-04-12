be362c933657ffa5c0c9f41c0d41ebb925fb2e3a
Guillaume DESSAIN <guillaume.dessain@epita.fr>
2019-04-12 12:52:04 +0200

    Add Color Refinement List Widget (#14)
    * Add Color Refinement List Widget
    
    * Update README.md
    
    Specify how should the record color attribute should be formatted.

---
+++ color-refinement-list/react-instantsearch/src/lib/ColorRefinementList.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { connectRefinementList } from "react-instantsearch-dom";
+
+import './ColorRefinementList.css';
+
+class ColorRefinementList extends Component {
+    render() {
+        return (
+            <div className="ais-ColorRefinementList">
+                <ul className="ais-ColorRefinementList-container">
+                    {this.props.searchable ? (
+                        <li>
+                            <input
+                                type="search"
+                                onChange={event =>
+                                    this.props.searchForItems(event.currentTarget.value)
+                                }
+                            />
+                        </li>
+                    ) : null}
+                    {this.props.items.map(item => {
+                        const color =
+                            item.label.split(";")[0].length === 4
+                                ? item.label.split(";")[0] +
+                                item.label.split(";")[0].slice(1, 4)
+                                : item.label.split(";")[0];
+
+                        return (
+                            <a
+                                href={this.props.createURL(item.value)}
+                                onClick={event => {
+                                    event.preventDefault();
+                                    this.props.refine(item.value);
+                                }}
+                                key={item.label}
+                            >
+                                <li
+                                    status={item.isRefined ? "checked" : ""}
+                                    className="ais-ColorRefinementElement"
+                                    style={{
+                                        background: `linear-gradient(145deg, ${color} 53%, ${color}e6 47%)`
+                                    }}
+                                />
+                            </a>
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default connectRefinementList(ColorRefinementList);
